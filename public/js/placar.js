95f08936561212a08c953a70acebb29d69b65085
Metamorph Dev <42251514+gtonussi@users.noreply.github.com>
2019-04-26 00:10:08 -0300

    Add files via upload

---
+++ public/js/placar.js
@@ -0,0 +1,55 @@
+console.log("O arquivo javascript placar.js foi carregado");
+
+function inserePlacar(){
+  var corpoTabela = $(".placar").find("tbody");
+  var usuario = "Jogador";
+  var numPalavras = $("#contador-palavras").text();
+
+  var linha = novaLinha(usuario, numPalavras);
+  linha.find(".botao-remover").click(removeLinha);
+
+  corpoTabela.prepend(linha);
+
+  $(".placar").slideDown(500);
+
+  console.log("rodou inserePlacar")
+}
+
+function scrollPlacar(){
+  var posicaoPlacar = $(".placar").offset().top;
+  $("body").animate({
+    scrollTop: posicaoPlacar+"px"
+  }, 1000);
+}
+
+function novaLinha(usuario, numPalavras){
+  var linha = $("<tr>");
+  var colunaUsuario = $("<td>").text(usuario);
+  var colunaPalavras = $("<td>").text(numPalavras);
+  var colunaRemover = $("<td>");
+
+  var link = $("<a>").addClass("botao-remover").attr("href", "#");
+  var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+  link.append(icone);
+  colunaRemover.append(link);
+  linha.append(colunaUsuario);
+  linha.append(colunaPalavras);
+  linha.append(colunaRemover);
+
+  return linha;
+}
+
+function removeLinha(){
+  event.preventDefault();
+  $(this).parent().parent().fadeOut(500);
+  setTimeout(function(){
+    $(this).parent().parent().remove();
+  }, 500);  
+}
+
+$("#botao-placar").click(mostraPlacar);
+
+function mostraPlacar(){
+  $(".placar").stop().toggle();
+}
